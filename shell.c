b944cefef1bc1c8c8a168265a80a91d1a1676955
Ot-Sara <saraotmani95@gmail.com>
2023-05-22 09:17:39 -0700

    1.shell.c

--- shell.c
+++ shell.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
 /**
  * main - Entry point
- * @ac: the number of items in av
- * @av: an array of strings
+ *
  * Return: 0 (SUCCESS)
  */
 
-int main()
+int main(void)
 {
-	size_t size_BUF = 0;
-	int num = 0;
-	char *BUF = NULL;
+	size_t command_size = 0;
+	ssize_t num;
+	pid_t pid;
+	int status;
+	char *command = NULL;
 
-	write(1, "$ ", 2);
-	num = getline(&BUF, &size_BUF, stdin);
+	while (1)
+	{
+		printf("$ ");
+	num = getline(&command, &command_size, stdin);
 	if (num == EOF)
+	{
 		perror("getline");
-	BUF[num - 1] = '\0';
-	printf("%s\n", BUF);
+		break;
+	}
+	command[num - 1] = '\0';
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execlp(command, command, NULL) == -1)
+		{
+			perror("execlp");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		wait(&status);
+	}
+	printf("Exit\n");
 	return (0);
 }
