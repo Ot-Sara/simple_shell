c961239f6096cb0efdbe4004539764b2023f21d3
Ot-Sara <saraotmani95@gmail.com>
2023-05-23 08:05:35 -0700

    5.shell.c

--- shell.c
+++ shell.c
@@ -6,16 +6,21 @@
 #include <sys/stat.h>
 
 /**
- * _getenv - 
- * @name:
+ * _getenv - retrieves the value of an environment variable
+ * @name: the name of the environment variable we want to
+ * retrieve the value for
  * Return: the value
  */
+
+/*
+ * Declaration for accessing environment variables
+ */
+extern char **environ;
 
 char *_getenv(const char *name)
 {
 	int i = 0;
 	char *key;
-	extern char **environ;
 
 	while (environ[i])
 	{
@@ -25,43 +30,46 @@ char *_getenv(const char *name)
 		i++;
 	}
 	return (NULL);
+}
 
+/**
+ * get_command - finds the path and stick it to the command
+ * @cmd: the command entered by the user
+ * Return: the whole path
+ */
+
+char *get_command(char *cmd)
+{
+	char *P = _getenv("PATH");
+	char *tok;
+	char *command;
+	struct stat st;
+
+	tok = strtok(P, ":");
+	while (tok)
+	{
+		command = malloc(strlen(tok) + strlen(cmd) + 2);
+		strcpy(command, tok);
+		strcat(command, "/");
+		strcat(command, cmd);
+
+		if (stat(command, &st) == 0)
+			return (command);
+
+		free(command);
+		tok = strtok(NULL, ":");
+	}
+	return (NULL);
 }
 
 /**
  * split_string - splits and returns an array of strings
  * @str: the string to return
+ * @delimiter: the delimiter to check
  * Return: an array of strings
  */
 
-
-char *get_command(char *command)
-{
-        char *path = _getenv("PATH");
-        char *token;
-        char *cmd_full;
-        struct stat st;
-
-        token = strtok(path, ":");
-        while (token)
-        {
-                cmd_full = malloc(strlen(token) + strlen(command) + 2);
-                strcpy(cmd_full, token);
-                strcat(cmd_full, "/");
-                strcat(cmd_full, command);
-
-                if (stat(cmd_full, &st) == 0)
-                        return (cmd_full);
-
-
-                free(cmd_full);
-                token = strtok(NULL, ":");
-        }
-        return (NULL);
-}
-
-
-char **split_string(char *str, char *del)
+char **split_string(char *str, char *delimiter)
 {
 	char *tok, **toks;
 	int n = 0;
@@ -72,70 +80,66 @@ char **split_string(char *str, char *del)
 			perror("malloc");
 			exit(EXIT_FAILURE);
 		}
-	tok = strtok(str, del);
+	tok = strtok(str, delimiter);
 
 	while (tok)
 	{
 		toks[n] = tok;
-		tok = strtok(NULL, del);
+		tok = strtok(NULL, delimiter);
 		n++;
 	}
 	toks[n] = NULL;
 	return (toks);
 }
+
 /**
  * main - Entry point
- *
+ * @ac: the number of items in av
+ * @av: an array of strings
+ * @env: the environment path
  * Return: 0 (SUCCESS)
  */
 
 int main(int ac, char **av, char **env)
 {
 
-	size_t command_size = 0;
+	size_t str_size = 0;
 	ssize_t num;
 	pid_t pid;
 	int status;
-	char *command = NULL;
+	char *str = NULL;
 	char **toks = NULL;
-	char *cmd;
-
 	(void)ac;
 	(void)av;
 
 	while (1)
 	{
 		write(1, "#cisfun$ ", 9);
-	num = getline(&command, &command_size, stdin);
+	num = getline(&str, &str_size, stdin);
 	if (num == -1)
 	{
-		perror("getline");
-		exit(1);
-	}
-	command[num - 1] = '\0';
-	toks = split_string(command, " \t\n");
+		write(1, "\n", 1);
+		exit(1); }
+	str[num - 1] = '\0';
+	toks = split_string(str, " ");
 
 	if (strcmp(toks[0], "exit") == 0)
-		exit (0);
-
+		exit(0);
 	pid = fork();
 	if (pid < 0)
 	{
 		perror("fork");
-		exit(EXIT_FAILURE);
-	}
+		exit(EXIT_FAILURE); }
 	else if (pid == 0)
 	{
-		cmd = get_command(toks[0]);
-		
-		if (cmd)
-			execve(cmd, toks, env);
+		str = get_command(toks[0]);
+		if (str)
+			execve(str, toks, env);
 		else
-			printf("Command not found\n");
-		exit(0);
-	}
+		exit(0); }
 	else
-		wait(&status);
-	}
+		wait(&status); }
+	free(str);
+	free(toks);
 	return (0);
-} 
+}
