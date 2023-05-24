ac8d8435a1d76bab40489442088b2c73ec78f2ae
apollotried <safeapollogemail@gmail.com>
2023-05-24 08:36:52 -0700

    update

--- main.h
+++ main.h
@@ -7,7 +7,6 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include "env.h"
 #include <fcntl.h>
 
 extern char **environ;
--- shell.c
+++ shell.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "functions.c"
 
 /**
  * main - Entry point
