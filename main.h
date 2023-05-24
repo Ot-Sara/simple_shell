59f611f5606657e827f7c7e71566b53d98c835af
Ot-Sara <saraotmani95@gmail.com>
2023-05-24 08:45:55 -0700

    shell.c

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include "env.h"
+#include <fcntl.h>
+
+extern char **environ;
+
+char *_getenv(const char *name);
+char *get_command(char *cmd);
+char **split_string(char *str, char *delimiter);
+
+#endif
--- shell.c
+++ shell.c
@@ -1,91 +1,5 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include "env.h"
-#include <fcntl.h>
-/**
- * _getenv - return the value of a spesific key
- * @name: the key to manipulate
- * Return: char*
- */
+#include "main.h"
 
-char *_getenv(const char *name)
-{
-	int l = 0;
-	char *key;
-
-	while (environ[l])
-	{
-		key = strtok(environ[l], "=");
-		if (strcmp(name, key) == 0)
-			return (strtok(NULL, "\n"));
-		l++;
-	}
-	return (NULL);
-}
-/**
- * get_command - finds the path and stick it to the command
- * @cmd: the command entered by the user
- * Return: the whole path
- */
-
-char *get_command(char *cmd)
-{
-	char *P = _getenv("PATH");
-	char *tok;
-	char *command;
-	struct stat st;
-
-	tok = strtok(P, ":");
-	while (tok)
-	{
-		command = malloc(strlen(tok) + strlen(cmd) + 2);
-		strcpy(command, tok);
-		strcat(command, "/");
-		strcat(command, cmd);
-
-		if (stat(command, &st) == 0)
-			return (command);
-
-		free(command);
-		tok = strtok(NULL, ":");
-	}
-	return (NULL);
-}
-
-/**
- * split_string - splits and returns an array of strings
- * @str: the string to return
- * @delimiter: the delimiter to check
- * Return: an array of strings
- */
-
-char **split_string(char *str, char *delimiter)
-{
-	char *tok, **toks;
-	int n = 0;
-
-	toks = malloc(sizeof(char *) * 1024);
-		if (toks == NULL)
-		{
-			perror("malloc");
-			exit(EXIT_FAILURE);
-		}
-	tok = strtok(str, delimiter);
-
-	while (tok)
-	{
-		toks[n] = tok;
-		tok = strtok(NULL, delimiter);
-		n++;
-	}
-
-	toks[n] = NULL;
-	return (toks);
-}
 
 /**
  * main - Entry point
@@ -97,23 +11,22 @@ char **split_string(char *str, char *delimiter)
 
 int main(int ac, char **av, char **env)
 {
-	char *str = NULL;
+	char *str = NULL, **toks = NULL, *command;
 	size_t str_size = 0;
 	ssize_t num;
-	char **toks;
 	pid_t pid;
-	int status;
+	int status, execve_result;
 	(void)ac;
 	(void)av;
 
 	while (1)
 	{
-		write(1, "#cisfun$ ", 9);
+		if (isatty(STDIN_FILENO))
+			write(1, "#cisfun$ ", 9);
 		num = getline(&str, &str_size, stdin);
 		if (num == -1)
 		{
-			write(1, "\n", 1);
-			exit(1); }
+			exit(EXIT_SUCCESS); }
 		str[num - 1] = '\0';
 		toks = split_string(str, " ");
 		if (strcmp(toks[0], "exit") == 0)
@@ -123,17 +36,19 @@ int main(int ac, char **av, char **env)
 		{
 			perror("fork");
 			exit(EXIT_FAILURE); }
-		if (pid == 0)
+		else if (pid == 0)
 		{
-			str = get_command(toks[0]);
-			if (str)
-				execve(str, toks, env);
+			command = get_command(toks[0]);
+			if (command)
+				execve_result = execve(command, toks, env);
+			else if (execve_result == -1)
+			{
+				perror("execve");
+				exit(EXIT_FAILURE); }
 			else
-			exit(0); }
+				exit(0); }
 		else
 			wait(&status);
-		free(toks);
-	}
+		free(toks); }
 	free(str);
-	return (0);
-}
+	return (0); }
