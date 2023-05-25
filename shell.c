9aa252d204ae80ea25f2c16b6045ae859597bf12
Ot-Sara <saraotmani95@gmail.com>
2023-05-24 18:37:43 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,8 +1,63 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+
+/**
+ * _getenv - return the value of a spesific key
+ * @name: the key to manipulate
+ * Return: char*
+ */
+
+char *_getenv(const char *name)
+{
+	int i = 0;
+	char *key;
+
+	while (environ[i])
+	{
+		key = strtok(environ[i], "=");
+		if (strcmp(name, key) == 0)
+			return (strtok(NULL, "\n"));
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * get_command - finds the path and stick it to the command
+ * @cmd: the command entered by the user
+ * Return: the whole path
+ */
+
+char *get_command(char *cmd)
+{
+	char *P = _getenv("PATH");
+	char *tok;
+	char *command;
+	struct stat st;
+
+	tok = strtok(P, ":");
+	while (tok)
+	{
+		command = malloc(strlen(tok) + strlen(cmd) + 2);
+		if (!command)
+		{
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+		strcpy(command, tok);
+		strcat(command, "/");
+		strcat(command, cmd);
+		if (stat(command, &st) == 0)
+			return (command);
+		free(command);
+		tok = strtok(NULL, ":");
+	}
+	return (NULL);
+}
 
 /**
  * split_string - splits and returns an array of strings
@@ -10,6 +65,7 @@
  * @delimiter: the delimiter to check
  * Return: an array of strings
  */
+
 char **split_string(char *str, char *delimiter)
 {
 	char *tok, **toks = NULL;
@@ -39,17 +95,21 @@ char **split_string(char *str, char *delimiter)
 
 /**
  * main - entry point
- *
+ * @ac: the number of items in av
+ * @av: an array of strings
+ * @env: the environment path
  * Return: 0 (SUCCESS)
  */
 
-int main(void)
+int main(int ac, char **av, char **env)
 {
 	size_t command_size = 0;
 	ssize_t num;
 	pid_t pid;
-	int status;
+	int status, execve_result;
 	char *command = NULL, **toks = NULL;
+	(void)ac;
+	(void)av;
 
 	while (1)
 	{
@@ -58,28 +118,28 @@ int main(void)
 		if (num == EOF)
 		{
 			perror("getline");
-			exit(EXIT_SUCCESS);
-		}
+			exit(EXIT_SUCCESS); }
 		command[num - 1] = '\0';
 		toks = split_string(command, " ");
+		if (strcmp(toks[0], "exit") == 0)
+			exit(0);
 		pid = fork();
 		if (pid < 0)
 		{
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
+			exit(EXIT_FAILURE); }
 		else if (pid == 0)
 		{
-			if (execve(command, toks, NULL) == -1)
+			if (command)
+				execve_result = execve(command, toks, env);
+			else if (execve_result == -1)
 			{
 				perror("execve");
-				exit(EXIT_FAILURE);
-			}
-		}
+				exit(EXIT_FAILURE); }
+			else
+				exit(0); }
 		else
 			wait(&status);
-	}
-	printf("Exit\n");
-	free(toks);
+		free(toks); }
+	free(command);
 	return (0);
 }
